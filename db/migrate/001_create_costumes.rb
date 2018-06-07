944fa959cc4b140f186ea7f473999ecc0236ec41
Everett <everett.bowline@gmail.com>
2018-06-07 01:30:56 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,6 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
-    add_table :costumes do |t|
+    create_table :costumes do |t|
       t.string :name
       t.float :price
       t.string :size
