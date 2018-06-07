2c331795c0c4468fabe7eb038fdff5e2b42c480d
Everett <everett.bowline@gmail.com>
2018-06-07 01:10:20 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,13 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouse < ActiveRecord::Migration
+  def change
+    add_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :inventory
+      t.integer :employees
+      t.string  :stillopen
+      t.time :openingtime
+      t.time :closingtime
+    end
+  end
+end
