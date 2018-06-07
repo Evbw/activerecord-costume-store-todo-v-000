9c1f00de241ff31e1210c634c7b46045dde5a7f8
Everett <everett.bowline@gmail.com>
2018-06-07 01:39:01 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,14 +1,14 @@
-#class CreateCostumeStore < ActiveRecord::Migration[5.1]
-#  def change
-#    create_table :costume_stores do |t|
-#      t.string :name
-#      t.string :location
-#      t.integer :inventory
-#      t.integer :employees
-#      t.boolean  :stillopen
-#      t.time :openingtime
-#      t.time :closingtime
-#      t.timestamps
-#    end
-#  end
-#end
+class CreateCostumeStore < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :inventory
+      t.integer :employees
+      t.boolean  :stillopen
+      t.time :openingtime
+      t.time :closingtime
+      t.timestamps
+    end
+  end
+end
