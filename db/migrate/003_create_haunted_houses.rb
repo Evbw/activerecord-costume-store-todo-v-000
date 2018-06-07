0a6c85be3f27b69304e63feebec1384b2fe67ea8
Everett <everett.bowline@gmail.com>
2018-06-07 01:25:27 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,14 +1,14 @@
-class CreateHauntedHouse < ActiveRecord::Migration[5.1]
-  def change
-    add_table :haunted_house do |t|
-      t.string :name
-      t.string :location
-      t.string :them
-      t.integer :price
-      t.string  :familyfriendly
-      t.datetime :openingdate
-      t.datetime :closingdate
-      t.text :description
-    end
-  end
-end
+#class CreateHauntedHouse < ActiveRecord::Migration[5.1]
+#  def change
+#    add_table :haunted_house do |t|
+#      t.string :name
+#      t.string :location
+#      t.string :them
+#      t.integer :price
+#      t.string  :familyfriendly
+#      t.datetime :openingdate
+#      t.datetime :closingdate
+#      t.text :description
+#    end
+#  end
+#end
